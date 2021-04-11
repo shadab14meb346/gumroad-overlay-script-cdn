5b16df9d02b11a9e3e2356441670cdb0b73a63fb
shadab <shadab14meb346@gmail.com>
2021-04-11 19:10:31 +0530

    add script to be available from cdn

---
+++ gumroad-overlay.js
@@ -0,0 +1,8 @@
+(function (document, script) {
+	script = document.createElement("script");
+	script.setAttribute(
+		"src",
+		"https://cdn.jsdelivr.net/gh/shadab14meb346/gumroad-overlay-script-cdn/gumroad-overlay-assets.js"
+	);
+	document.getElementsByTagName("head")[0].appendChild(script);
+})(document);
